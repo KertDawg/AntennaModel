012da919c41aeab1b14200c82dd3e62f762f443c
Kertis Henderson <kertishenderson@gmail.com>
2022-09-12 10:34:25 -0400

    Added client build command

--- docker/Build.ps1
+++ docker/Build.ps1
@@ -2,4 +2,11 @@
 $env:DOCKER_BUILDKIT=0
 $env:COMPOSE_DOCKER_CLI_BUILD=0
 
+
+#  Build the client.
+cd ../client
+quasar build
+cd ..
+
+#  Build the docker image.
 docker build -t kertdawg/antenna-model .
