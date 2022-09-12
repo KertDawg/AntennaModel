3c02733ef770d652952afe304e90eee4c776121c
Kertis Henderson <kertishenderson@gmail.com>
2022-09-12 09:48:49 -0400

    Added web client project

---
+++ client/src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "LayoutMain",
+
+  components: {
+  },
+
+  mounted()
+  {
+   if (this.$route.path === "/")
+    {
+      this.$router.push({ name: "home" });
+    }
+  },
+};
+</script>
---
+++ client/src/pages/Error404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    Bad URL
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Page404",
+};
+</script>
