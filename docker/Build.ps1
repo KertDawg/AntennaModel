ebaa05b3c875830efeeb4acedd8b808bff7211d0
Kertis Henderson <kertishenderson@gmail.com>
2022-09-12 10:52:30 -0400

    Updated scripts

--- docker/Build.ps1
+++ docker/Build.ps1
@@ -3,10 +3,5 @@ $env:DOCKER_BUILDKIT=0
 $env:COMPOSE_DOCKER_CLI_BUILD=0
 
 
-#  Build the client.
-cd ../client
-quasar build
-cd ..
-
 #  Build the docker image.
 docker build -t kertdawg/antenna-model .
