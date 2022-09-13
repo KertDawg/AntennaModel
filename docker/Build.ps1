5203b27262cba1216a3ce98c9c7ef5885d486084
Kertis Henderson <kertishenderson@gmail.com>
2022-09-13 15:23:45 -0400

    Updated buils script to git pull.

--- docker/Build.ps1
+++ docker/Build.ps1
@@ -3,5 +3,14 @@ $env:DOCKER_BUILDKIT=0
 $env:COMPOSE_DOCKER_CLI_BUILD=0
 
 
+#  Does the repo exist?
+If (Test-Path -Path ./README.md)
+{
+    git clone https://github.com/KertDawg/AntennaModel.git
+}
+
+#  Update the code.  (I suppose this file won't update, but I'm OK with that.)
+git pull
+
 #  Build the docker image.
 docker build -t kertdawg/antenna-model .
