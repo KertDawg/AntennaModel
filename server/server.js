475ccb3914b7abe72dfa71f5dc3c14405fd99acd
Kertis Henderson <kertishenderson@gmail.com>
2022-09-12 20:46:22 -0400

    Added static server for client

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 
 
 
-var app = express();
+var eserver = express();
 var router = express.Router();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
+eserver.use(bodyParser.json());
+eserver.use(bodyParser.urlencoded({ extended: true }));
+eserver.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Token");
     next();
@@ -19,9 +19,16 @@ router.get("/", function (req, res) {
     res.json({ message: "The API is here." });
 });
 
-
-app.use("/", router);
+eserver.use("/", router);
 
 app.listen(process.env.PORT || 3000, "0.0.0.0", function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+
+var eclient = express();
+eclient.use(express.static("../client/dist/spa"))
+
+eclient.listen(process.env.PORT || 4000, "0.0.0.0", function() {
+    console.log("Client listening on port %d in %s mode", this.address().port, app.settings.env);
 });
